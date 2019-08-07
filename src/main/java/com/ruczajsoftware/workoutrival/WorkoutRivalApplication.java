aa911e3e4e48b204dba55f6b1d243bbe7f3cdcc5
danbaziak <danielbaziak@gmail.com>
2019-08-07 22:18:59 +0200

    Initial commit

---
+++ src/main/java/com/ruczajsoftware/workoutrival/WorkoutRivalApplication.java
@@ -0,0 +1,13 @@
+package com.ruczajsoftware.workoutrival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutRivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutRivalApplication.class, args);
+	}
+
+}
